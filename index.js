909f58f41be0e7aaafe6efa20b00725843ec72b1
Navi Kaur <navdeep9520@gmail.com>
2021-08-29 10:22:00 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+    let ans= a+b
+    return ans
+}
+
+function subtract(a,b){
+    let ans= a-b
+    return ans
+}
+
+function multiply(a,b){
+    let ans=a*b
+    return ans
+}
+
+function divide(a,b){
+    let ans=a/b
+    return ans
+}
+function increment(n){
+    n++
+    return n
+}
+function decrement(n){
+    n--
+    return n
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+} 
